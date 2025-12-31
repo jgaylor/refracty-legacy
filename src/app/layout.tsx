4f7ae34c28269fe43e8b0de9751d650a546bd0a0
jgaylor <15039182+jgaylor@users.noreply.github.com>
2025-12-30 21:31:59 -0600

    attempt to fix SSL error on Arc with viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,12 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Refracty",
   description: "Refracty application",
-  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+};
+
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default async function RootLayout({
